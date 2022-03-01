5bd786ec6c1056e8f285729a5d977bd7e3ad437b
Spencer McKnight <spencer.mcknight.g@gmail.com>
2022-03-01 14:04:17 +1100

    Remove recursive function

--- src/components/table.tsx
+++ src/components/table.tsx
@@ -11,34 +11,18 @@ const isDisplayed: (arg0: DonationEntry) => boolean = (value: DonationEntry) => 
     return value.isPublic === true;
 }
 
-//recursive function to avoid writing over variables
-const mapHTML: (displayData: DonationEntry[], listItems: JSX.Element[], index: number) => React.FC | JSX.Element[]
-    = (displayData: DonationEntry[], listItems: JSX.Element[], index: number) => {
-        //create html to add to newlist
-        const newListItem: JSX.Element = <li key={index}>{displayData[index].amount}</li>
-
-        //add html to newList
-        const newListItems: JSX.Element[] = [...listItems, newListItem];
-
-        //pass newList into recursive function
-        const newIndex: number = index + 1;
-
-        if (newIndex < displayData.length) {
-            return mapHTML(displayData, newListItems, newIndex);
-        } else {
-            return newListItems;
-        }
-    }
-
 const getTableHTML: (arg0: DonationEntry[]) => JSX.Element = (dataset: DonationEntry[]) => {
     const displayData: DonationEntry[] = dataset.filter(isDisplayed);
 
-    const HTML = mapHTML(displayData, [], 0)
+    const listNodes = displayData.map(function (item, i) {
+        return <li key={i}>{item.amount}</li>
+    })
 
-    return <ul>{HTML}</ul>;
+    return <ul>{listNodes}</ul>;
 }
 
 const Table: React.FC<Props> = ({ dataset }) => {
+
     const tableHTML = getTableHTML(dataset);
 
     return (
